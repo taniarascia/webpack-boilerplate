6e6992d0111a8d536b5cce9ca96c8d28fdacf78a
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2020-08-22 16:52:55 -0500

    Optimized images.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const ImageMinPlugin = require("imagemin-webpack");
 
 module.exports = merge(common, {
   mode: 'production',
@@ -25,6 +26,33 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: 'styles/[name].[contenthash].css',
       chunkFilename: '[id].css',
+    }),
+
+    new ImageMinPlugin({
+      bail: false, // Ignore errors on corrupted images
+      cache: true,
+      imageminOptions: {
+        // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["mozjpeg", { quality: 80 }],
+          ["optipng", { optimizationLevel: 5 }],
+          [
+            "svgo",
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
     }),
   ],
   module: {
