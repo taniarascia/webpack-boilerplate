6c840a1751558032b38290ccceb0b08e4d27358d
Jamie Warburton <jamie@hexdigital.com>
2020-11-05 17:40:45 +0000

    Update css minimizer optimisation for Webpack 5
    Optimize css assets plugin recommends using css-minimizer-webpack-plugin for Webpack 5. This commit swaps them. See: https://github.com/NMFR/optimize-css-assets-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,7 +3,7 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -42,7 +42,7 @@ module.exports = merge(common, {
   },
   optimization: {
     minimize: true,
-    minimizer: [new OptimizeCssAssetsPlugin(), "..."],
+    minimizer: [new CssMinimizerPlugin(), "..."],
     // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
     // instead of having their own. This also helps with long-term caching, since the chunks will only
     // change when actual code changes, not the webpack runtime.
