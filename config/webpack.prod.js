8045eb9cdef1264e111af7fa4b7d76e5ae04982c
thegoodcode <rhapsody@tuta.io>
2021-10-07 22:15:49 +0100

    Fix paths issue in production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/',
+    publicPath: './',
     filename: 'js/[name].[contenthash].bundle.js',
   },
   module: {
